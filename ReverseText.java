2da4789ecfca150f6afadbe4994a8e15f7d26505
tito <tito657175>
2024-08-29 14:49:37 -0700

    Changed loop to a more easily read format

--- ReverseText.java
+++ ReverseText.java
@@ -9,15 +9,21 @@ public class ReverseText {
    }
 
 // String Reverse Method
-public static String reverseText(String s) {
-   String[] words = s.split(" ");  // slits string into words with space
+public static String reverseText(String s) { //Accepts input variable s
+   String[] words = s.split(" ");  // splits string into Array and adds spaces
    StringBuilder reversedSentence = new StringBuilder();
 
-   for (String word : words) {
+   /* for (String word : words) { //Cool short hand format, alternate to (string word;)
        StringBuilder reversedWord = new StringBuilder(word);
        reversedWord.reverse();  // Reverse the individual word
        reversedSentence.append(reversedWord.toString()).append(" "); // Add the reversed word to the sentence
-   }
+   } */
+
+   for (int i = 0; i < words.length; i++) {
+      StringBuilder reversedWord = new StringBuilder(words[i]);
+      reversedWord.reverse();  // Reverse the individual word
+      reversedSentence.append(reversedWord.toString());
+
 
    return reversedSentence.toString().trim();  // Convert to string and trim any extra space at the end
 }
