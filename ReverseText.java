23e69e68e5e609ffa8c714f9c5656d243535b427
tito <tito657175>
2024-08-29 16:51:15 -0700

    Additional comments for clarity

--- ReverseText.java
+++ ReverseText.java
@@ -8,24 +8,19 @@ public class ReverseText {
       System.out.print(reverseText(s) + "\n");
    }
 
-// String Reverse Method
+// Reverse Method
 public static String reverseText(String s) { //Accepts input variable s
-   String[] words = s.split(" ");  // splits string into Array and adds spaces
+   String[] words = s.split(" ");  // Splits string into Array and adds spaces
    StringBuilder reversedSentence = new StringBuilder();
 
-   /* for (String word : words) { //Cool short hand format, alternate to (string word;)
+   for (String word : words) { //Cool short hand format, alternate to for (int i = 0; i < words.length; i++)
        StringBuilder reversedWord = new StringBuilder(word);
        reversedWord.reverse();  // Reverse the individual word
        reversedSentence.append(reversedWord.toString()).append(" "); // Add the reversed word to the sentence
-   } */
-
-   for (int i = 0; i < words.length; i++) {
-      StringBuilder reversedWord = new StringBuilder(words[i]);
-      reversedWord.reverse();  // Reverse the individual word
-      reversedSentence.append(reversedWord.toString());
-
-
-   return reversedSentence.toString().trim();  // Convert to string and trim any extra space at the end
+   }
+   return reversedSentence.toString().trim();  // Convert back to string and trim space at the end
 }
 }
+
+
 
